1fd63dcd8c5743aa4750cebfa51b566c55c5ffc5
Nyny2121 <nyny830@gmail.com>
2020-01-22 21:10:31 -0600

    Add files via upload

---
+++ ProgramTemplate.java
@@ -0,0 +1,19 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// CS I : Program (number)
+// (Program Name)
+// Semester : 1
+//
+// Kaden Scroggins
+// 60992
+//
+// Description:
+// (Program Description)
+
+public class ProgramX {
+
+  public static void main(String[] args) {
+
+    System.out.println( Math.PI );
+    
+  }
+}
